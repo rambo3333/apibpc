81858289962296833fc3b75bf2d004e62ffa8413
rambo <15019207291@163.com>
2018-09-11 17:31:25 +0800

    添加部分购买方案接口

--- app/Http/Controllers/Api/CmodelsController.php
+++ app/Http/Controllers/Api/CmodelsController.php
@@ -60,8 +60,10 @@ class CmodelsController extends Controller
 
         //系统指导价
         $data['guide_price'] = $cmodel->guide_price;
+
         //成交价
         $data['transaction_price'] = $data['guide_price'] - $cmodel->pre_amount;
+
         //购置税：购车款/(1+16%) X 购置税率(10%) (当车价优惠幅度超过指导价的 8% 时，购车款 = 指导价 * 92%)
         $gck = $data['transaction_price'];
         $pre_amount = intval($data['guide_price'] * 0.08);
@@ -69,14 +71,39 @@ class CmodelsController extends Controller
             $gck = intval($data['guide_price'] * 0.92);
         }
         $data['gzs'] = $gck / 1.16 * 0.1;
+
         //上牌费
         $data['spf'] = config('car.spf');
+
         //车船税(不足一年按当年剩余月算)
         $syy = date('n');
-        $ccs =
+        $syy_rate = ((12 - $syy + 1) / 12);
+        $pl = intval($cmodel->pl * 10);
+        if ($pl <= 10) {
+            $ccs = config('car.ccs1');
+        } elseif (10 < $pl && $pl <= 16) {
+            $ccs = config('car.ccs2');
+        } elseif(16 < $pl && $pl <= 20) {
+            $ccs = config('car.ccs3');
+        } elseif (20 < $pl && $pl <= 25) {
+            $ccs = config('car.ccs4');
+        } elseif (25 < $pl && $pl <= 30) {
+            $ccs = config('car.ccs5');
+        } elseif (30 < $pl && $pl <= 40) {
+            $ccs = config('car.ccs6');
+        } elseif (40 < $pl) {
+            $ccs = config('car.ccs7');
+        }
+        $data['ccs'] = $ccs * $syy_rate;
 
+        //交强险
+        if ($cmodel->zw < 6) {
+            $data['jqx'] = config('car.jqx1');
+        } else {
+            $data['jqx'] = config('car.jqx2');
+        }
 
-
+        //商业险（全款）
         if ($program_id == 1) {
 
         }
