98face4fac0dce484e4ec27d9936d2bff1fd9078
wangsz <15019207291@163.com>
2018-09-19 17:24:44 +0800

    添加订单文件

---
+++ app/Http/Controllers/Api/OrdersController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\Api\OrderRequest;
+use EasyWeChat\Factory;
+
+class OrdersController extends Controller
+{
+
+}
