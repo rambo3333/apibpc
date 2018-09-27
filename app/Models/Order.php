aa7c6d70f76aa03b7f9f26e4315cfe702a9cb9b5
wangsz <15019207291@163.com>
2018-09-27 17:28:16 +0800

    创建订单模型

---
+++ app/Models/Order.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected static function boot()
+    {
+        parent::boot();
+        // 监听模型创建事件，在写入数据库之前触发
+        static::creating(function ($model) {
+            // 如果模型的 order_no 字段为空
+            if (!$model->order_no) {
+                // 调用 findAvailableNo 生成订单流水号
+                $model->order_no = static::findAvailableNo();
+                // 如果生成失败，则终止创建订单
+                if (!$model->order_no) {
+                    return false;
+                }
+            }
+        });
+    }
+
+    public static function findAvailableNo()
+    {
+        // 订单流水号前缀
+        $prefix = date('YmdHis');
+        for ($i = 0; $i < 10; $i++) {
+            // 随机生成 6 位的数字
+            $order_no = $prefix.str_pad(random_int(0, 999999), 6, '0', STR_PAD_LEFT);
+            // 判断是否已经存在
+            if (!static::query()->where('order_no', $order_no)->exists()) {
+                return $order_no;
+            }
+        }
+        \Log::warning('find order no failed');
+
+        return false;
+    }
+}
